263baaaa0b5f423fa1e029d1bce0d4708c9bebac
SzymonAK <szymon.kamil@gmail.com>
2015-05-11 21:37:17 +0200

    Skeleton

--- App/app.js
+++ App/app.js
@@ -1,1 +1,12 @@
-angular.module("ONPChanger", []);
+var app = angular.module("ONPChanger", ["ngRoute"]);
+app.config(["$routeProvider", "$locationProvider" , function ($routeProvider, $locationProvider){
+	$routeProvider
+		.when("/", {
+			templateUrl: "./views/home.html",
+			controller: "homeController"
+		})
+		.when("/about",{
+			templateUrl: "/views/about.html"
+		});
+	$locationProvider.html5Mode(true);
+}]);

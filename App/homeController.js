f2b5c925d306b5d2981678c35eb36cb05315206e
kszymanski <szymon.kamil@gmail.com>
2015-05-13 10:52:20 +0200

    first impl of infix to postfix

--- App/homeController.js
+++ App/homeController.js
@@ -55,10 +55,15 @@ app.controller("homeController", ["$scope", function ($scope) {
 	};
 
 	var alreadySelected = function () {
-		$scope.solution.expression = "Zaznaczyłeś te samą postać a wiec wynik jest ten sam";
-		$scope.solution.result = "info"
+		displayResult("Zaznaczyłeś te samą postać a wiec wynik jest ten sam","info");
+	}
+
+	var displayResult = function (msg, status) {
+		$scope.solution.expression = msg;
+		$scope.solution.result = status;
 		$scope.solution.display = true;
 	}
+
 
 	var inToPost = function () {
 		var reStr = $scope.expression;
@@ -67,18 +72,67 @@ app.controller("homeController", ["$scope", function ($scope) {
 		for (var k = 0, length = reStr.length; k < length; k++) {
 			// current char
 			var c = reStr[k];
-			if(c == " "){
+			// skip whitespaces
+			if (c == " ") {
 				console.log('whitespace');
 				continue;
 			}
-			if ($.inArray(c,operands) !== -1){
-				
+			// operands algoritm
+			if ($.inArray(c, operands) !== -1) { // if c is one of operands
+				if (c == ')') {
+					while (stack[stack.length - 1] != '(') {
+						output.push(stack.pop());
+					}
+					stack.pop();
+					continue;
+				}
+				if (c == '(') {
+					stack.push(c);
+					continue;
+				}
+
+				if (stack.length == 0 || operandsStrength[stack[stack.length - 1]] < operandsStrength[c]) { // for empty stack or if strength is higher just add it
+					stack.push(c);
+				} else {
+					while (operandsStrength[stack[stack.length - 1]] >= operandsStrength[c]) { // while strength is lower or equal
+						if (c == '^') { //for right site operand 
+							if (operandsStrength[stack[stack.length - 1]] > operandsStrength[c]) { // if lower 
+								output.push(stack.pop());
+								continue;
+							} else {
+								break;
+							}
+						} else {
+							output.push(stack.pop());
+							continue;
+						}
+					}
+					stack.push(c);
+				}
+			} else {
+				output.push(c);
 			}
-			console.log(c);
 
 		}
+		for (var k = 0, length = stack.length; k < length; k++) {
+			output.push(stack.pop());
+		}
+		var msg = "";
+		for (var k = 0, length = output.length; k < length; k++) {
+			msg += output[k];
+		}
+		displayResult(msg, 'success');
 	}
-	var operands = ['(',')','+']; 
 
+	var operands = ['(', ')', '+', '-', '/', '*', '^'];
+	var operandsStrength = {
+		'(': 0,
+		')': 0,
+		'+': 1,
+		'-': 1,
+		'*': 2,
+		'/': 2,
+		'^': 3
+	};
 
 }]);
